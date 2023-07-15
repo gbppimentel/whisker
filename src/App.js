62c6196e23d93c119574c70f594e6e41a3d98970
gbppimentel <111882471+gbppimentel@users.noreply.github.com>
2023-07-16 01:33:23 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Landing from "./Landing";
+import logosub from "./assets/logo/whisker-sub.png";
+import landing1 from "./assets/img/landing-1.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ position: "relative" }} className="App">
+      <img
+        src={logosub}
+        alt="logosub"
+        style={{
+          width: "258.315px",
+          height: "95px",
+          flexShrink: 0,
+          position: "absolute",
+          top: "20%",
+          left: "50%",
+          transform: "translate(-50%, -50%)",
+          zIndex: 3,
+        }}
+      />
+      <img
+        src={landing1}
+        alt="landing1"
+        style={{
+          width: "393px",
+          height: "553px",
+
+          position: "absolute",
+          top: "250px",
+          transform: "translate(-50%, -50%)",
+          zIndex: 2,
+        }}
+      />
+
+      <Landing />
     </div>
   );
 }
